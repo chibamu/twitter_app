664006714edb5dcf72df5d708527c2f07024ba6d
Ngoni D. Chibamu <dominic.chibamu@gmail.com>
2013-01-25 11:50:18 +0200

    Apparently not able to do anything--->force completes shit only

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitterApp::Application.config.secret_token = '97342697483a46a1fd0ec66093f86eb7c56dee6cf7fe42d3768d20a55cde7ee0519cf608d53c190248eed2fd13293af03b57be48fdcb49ba8c9ab82b6a55bbea'
